4ed16d4cb59ce3e5e98ef9f1f2b3d90367c11d83
marlonknupp <marlonjcc23@gmail.com>
2025-05-01 21:38:12 -0300

    primeiro commit

---
+++ brands/views.py
@@ -0,0 +1,18 @@
+from typing import Any
+from django.db.models.query import QuerySet
+from django.views.generic import ListView
+from . import models 
+
+class BrandListView(ListView):
+    model = models.Brand
+    template_name = 'brand_list.html'
+    context_object_name = 'brands'
+
+def get_queryset(self):
+    queryset = super().get_queryset()
+    name = self.request.GET.get('name')
+
+    if name:
+        queryset = queryset.filter(name__icontains=name)
+
+    return queryset
